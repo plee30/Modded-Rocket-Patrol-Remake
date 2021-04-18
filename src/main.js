bc506fd492bbe49a86ca5de283d5d070edd77293
Paul Lee <plee30@ucsc.edu>
2021-04-18 04:38:23 -0700

    Added comments for point breakdown

--- src/main.js
+++ src/main.js
@@ -14,6 +14,17 @@ let borderPadding = borderUISize / 3;
 let keyF, keyR, keyLEFT, keyRIGHT;
 
 
-// https://freesound.org/people/CosmicEmbers/sounds/160756/
-// https://freesound.org/people/apolloaiello/sounds/351876/
-// https://freesound.org/people/basedMedia/sounds/548105/
+// Ball throw sound effect:     https://freesound.org/people/CosmicEmbers/sounds/160756/
+// Dog bark sound effect:       https://freesound.org/people/apolloaiello/sounds/351876/
+// Bird squawking sound effect: https://freesound.org/people/basedMedia/sounds/548105/
+
+// Paul Lee
+// Catch!
+// 4/19/2021
+// Took about 10 hours to complete the project
+
+// Point Breakdown:
+// 60 - Redesigned artwork, UI, and sounds to change theme
+// 20 - New spaceship type that is smaller, faster, and worth more points
+// 10 - New animated sprite for Spaceship enemies
+// 10 - Implemented parallax scrolling
