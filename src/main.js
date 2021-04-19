b7bc858f5add202710ff41da415d4711b8d8a578
Paul Lee <plee30@ucsc.edu>
2021-04-19 16:14:26 -0700

    Added credits to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// Paul Lee
+// Catch!
+// 4/19/2021
+// Took about 10 hours to complete the project
+
+// Ball throw sound effect:     https://freesound.org/people/CosmicEmbers/sounds/160756/
+// Dog bark sound effect:       https://freesound.org/people/apolloaiello/sounds/351876/
+// Bird squawking sound effect: https://freesound.org/people/basedMedia/sounds/548105/
+
+// Point Breakdown:
+// 60 - Redesigned artwork, UI, and sounds to change theme
+// 20 - New spaceship type that is smaller, faster, and worth more points
+// 10 - New animated sprite for Spaceship enemies
+// 10 - Implemented parallax scrolling
+
 let config = { 
     type: Phaser.CANVAS, 
     width: 640, 
@@ -12,19 +27,3 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
-
-
-// Ball throw sound effect:     https://freesound.org/people/CosmicEmbers/sounds/160756/
-// Dog bark sound effect:       https://freesound.org/people/apolloaiello/sounds/351876/
-// Bird squawking sound effect: https://freesound.org/people/basedMedia/sounds/548105/
-
-// Paul Lee
-// Catch!
-// 4/19/2021
-// Took about 10 hours to complete the project
-
-// Point Breakdown:
-// 60 - Redesigned artwork, UI, and sounds to change theme
-// 20 - New spaceship type that is smaller, faster, and worth more points
-// 10 - New animated sprite for Spaceship enemies
-// 10 - Implemented parallax scrolling
